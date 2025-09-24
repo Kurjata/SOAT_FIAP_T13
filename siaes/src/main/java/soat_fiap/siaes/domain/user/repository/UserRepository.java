ae6f3250a086c9d02be682d3bf653e1d2dee7659
Vinicius Louzada <vinelouzada@gmail.com>
2025-09-24 08:02:36 -0300

    chore: add repository interface to user

--- siaes/src/main/java/soat_fiap/siaes/domain/user/repository/UserRepository.java
+++ siaes/src/main/java/soat_fiap/siaes/domain/user/repository/UserRepository.java
@@ -1,24 +1,13 @@
 package soat_fiap.siaes.domain.user.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import soat_fiap.siaes.domain.user.model.User;
 
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository  {
+    List<User> findAll();
+    User save(User user);
     Optional<User> findByLogin(String login);
-
-    @Query(value = """
-            SELECT EXISTS (
-                SELECT 1
-                FROM tb_users u
-                WHERE u.login = :login 
-                    OR u.document = :document
-            ) AS user_exists;
-            """, nativeQuery = true)
     boolean existsUserByLoginOrDocument(String login, String document);
 }
---
+++ siaes/src/main/java/soat_fiap/siaes/infrastructure/persistence/user/UserJpaRepository.java
@@ -0,0 +1,25 @@
+package soat_fiap.siaes.infrastructure.persistence.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import soat_fiap.siaes.domain.user.model.User;
+import soat_fiap.siaes.domain.user.repository.UserRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserJpaRepository extends JpaRepository<User, UUID>, UserRepository {
+    Optional<User> findByLogin(String login);
+
+    @Query(value = """
+            SELECT EXISTS (
+                SELECT 1
+                FROM tb_users u
+                WHERE u.login = :login 
+                    OR u.document = :document
+            ) AS user_exists;
+            """, nativeQuery = true)
+    boolean existsUserByLoginOrDocument(String login, String document);
+}
