d4092a25c4c161249aefa28b21886b09474e68a5
Vinicius Louzada <vinelouzada@gmail.com>
2025-09-24 08:24:36 -0300

    feat: allow route public to create user

--- siaes/src/main/java/soat_fiap/siaes/interfaces/user/dto/CreateUserRequest.java
+++ siaes/src/main/java/soat_fiap/siaes/interfaces/user/dto/CreateUserRequest.java
@@ -1,7 +1,6 @@
 package soat_fiap.siaes.interfaces.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import soat_fiap.siaes.domain.user.model.RoleEnum;
 import soat_fiap.siaes.domain.user.model.User;
@@ -11,10 +10,9 @@ public record CreateUserRequest(
         @NotBlank String name,
         @NotBlank String login,
         @NotBlank String password,
-        @NotNull RoleEnum role,
         @Document String document
 ) {
     public User toModel(PasswordEncoder encoder) {
-        return new User(name, login, encoder.encode(password), role, document);
+        return new User(name, login, encoder.encode(password), RoleEnum.CLIENT, document);
     }
 }
