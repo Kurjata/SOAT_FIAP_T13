8a39da0020cbf671284be0ae2b1bf81e45f1733a
Vinicius Louzada <vinelouzada@gmail.com>
2025-09-27 10:58:39 -0300

    chore: organizacao de pastas

--- siaes/src/main/java/soat_fiap/siaes/interfaces/user/dto/MessageDTO.java
+++
@@ -1,6 +0,0 @@
-package soat_fiap.siaes.interfaces.user.dto;
-
-public record MessageDTO(
-        String message
-) {
-}
--- siaes/src/main/java/soat_fiap/siaes/interfaces/user/dto/RefreshTokenDTO.java
+++
@@ -1,9 +0,0 @@
-package soat_fiap.siaes.interfaces.user.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshTokenDTO(
-        @NotBlank(message = "O refresh token não pode estar vazio")
-        String refreshToken
-) {
-}
